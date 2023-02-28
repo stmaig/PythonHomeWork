b82ee40bb0f15168a9f8e73185baca4ed006138d
Maksim Stolypinsky <msto@exante.eu>
2023-02-28 23:20:20 +0300

    initial commite

---
+++ homeworks/1 lesson.py
@@ -0,0 +1,25 @@
+# print("Hello World!")
+# name=input("Как твоё имя?")
+# print("Твое имя, ", name)
+
+# a=int(input("Введи число"))
+# print(a+2)
+
+
+
+# easy
+# age = int(input("Сколько тебе лет?"))
+# if age >= 18:
+#     print("Доступ разрешен")
+# else:
+#     print("Доступ запрещен")
+
+# medium
+# a = 999
+# while a < 0 or a > 10:
+#     a = int(input("Введи любое число от 0 до 10:"))
+#     print("неверное число!")
+# print(a**2)
+
+
+# hard
